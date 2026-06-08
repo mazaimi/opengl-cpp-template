96b65b90f41e96d628ec6a7d233d7442b3496808
Moustapha A. <m.azaimi@student.avans.nl>
2026-06-08 21:23:39 +0200

    added external library -- glfw and opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,37 @@
-#include <iostream>
-#include "MyLib.h"
+#include <GLFW/glfw3.h>
 
-int main() {
-  std::cout << "Hello World" << std::endl;
+int main(void)
+{
+  GLFWwindow *window;
 
-  MyLib lib_instance;
-  lib_instance.setNumber(2);
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
 
-  std::cout << "MyLib number: " << lib_instance.getNumber() << std::endl;
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
 
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window))
+  {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
   return 0;
 }
